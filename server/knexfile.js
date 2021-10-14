b28c177b04d8673b1c5b71496c9e917e6e3dbbd8
Vanderson Rodrigues <vanderson@tutamail.com>
2021-10-14 01:14:07 -0300

    Set knex configurations

---
+++ server/knexfile.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+    client: "pg",
+    connection: {
+        host: process.env.PSQL_HOST,
+        user: process.env.PSQL_USER,
+        database: process.env.PSQL_DATABASE,
+        password: process.env.PSQL_PASSWORD
+    }
+};
