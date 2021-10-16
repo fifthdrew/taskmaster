4761cd32c43abc7388300bb4f075fc624820a593
Vanderson Rodrigues <vanderson@tutamail.com>
2021-10-16 14:22:24 -0300

    Add UserModel to Context class

--- server/core/context.js
+++ server/core/context.js
@@ -1,8 +1,10 @@
 const Db = require('./db');
+const UserModel = require('./models/user');
 
 class Context {
     constructor() {
         this.db = new Db();
+        this.user = new UserModel(this);
     }
 }
 
