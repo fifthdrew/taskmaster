32b6266e140c34a22c5bb734a14f3d3ff71b03bc
Vanderson Rodrigues <vanderson@tutamail.com>
2021-10-31 23:02:15 -0300

    Add middleware to allow CORS

--- server/api/app.js
+++ server/api/app.js
@@ -6,6 +6,14 @@ const Context = require('../core/context');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'POST');
+
+    next();
+});
+
 app.get('/', (req, res) => res.send('Hello!'));
 
 app.post('/users', bodyPasser.json(), async(req, res) => {
